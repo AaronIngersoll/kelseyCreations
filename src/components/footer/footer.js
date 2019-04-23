286a18f11f0e1cfc853bf7e9fb47f5962e6668df
Aaron Ingersoll <flex.box.hero@gmail.com>
2019-04-23 11:33:01 -0600

    add Youtube link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,51 +1,57 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 export default class Footer extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-    return(
-        <div className="footer">
-            <div className="logo-footer">
-                <img src="../assets/logo/good-logo.png" alt="Logo"/>
-            </div>
-            <div className="links-wrapper">
-                <div className="nav-wrapper">
-
-                        <div className="nav-link-wrapper">
-                        <NavLink to="/" activeClassName="nav-link-active">Home</NavLink>
-                        </div>
-                        <div className="nav-link-wrapper">
-                        <NavLink to="/bio" activeClassName="nav-link-active">Bio</NavLink>
-                        </div>
-                        <div className="nav-link-wrapper">
-                        <NavLink to="/films" activeClassName="nav-link-active">Films</NavLink>
-                        </div>
-                        <div className="nav-link-wrapper">
-                        <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
-                        </div>
-
-                </div>
-            </div>
-
-            <div className="social-media-icons-wrapper">
-                <a href="https://www.instagram.com/kelseycreations/">
-                    <i className="fab fa-instagram"></i>
-                </a>
-    
-                <a href="https://vimeo.com/kelseycreations">
-                    <i className="fab fa-vimeo-square"></i>
-                </a>
-            </div>
-            <div className="copyright-wrapper">
-                &copy; 2019 KelseyCreations &#124; all rights reserved
-            </div>
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="footer">
+        <div className="logo-footer">
+          <img src="../assets/logo/good-logo.png" alt="Logo" />
         </div>
-    )};  
-}
+        <div className="links-wrapper">
+          <div className="nav-wrapper">
+            <div className="nav-link-wrapper">
+              <NavLink to="/" activeClassName="nav-link-active">
+                Home
+              </NavLink>
+            </div>
+            <div className="nav-link-wrapper">
+              <NavLink to="/bio" activeClassName="nav-link-active">
+                Bio
+              </NavLink>
+            </div>
+            <div className="nav-link-wrapper">
+              <NavLink to="/films" activeClassName="nav-link-active">
+                Films
+              </NavLink>
+            </div>
+            <div className="nav-link-wrapper">
+              <NavLink to="/contact" activeClassName="nav-link-active">
+                Contact
+              </NavLink>
+            </div>
+          </div>
+        </div>
 
-            
-                       
-   
+        <div className="social-media-icons-wrapper">
+          <a href="https://www.instagram.com/kelseycreations/">
+            <i className="fab fa-instagram" />
+          </a>
+          <a href="https://www.youtube.com/channel/UCoDRD6ug-7jVrtn486E4BQw/featured">
+            <i className="fab fa-youtube" />
+          </a>
+
+          <a href="https://vimeo.com/kelseycreations">
+            <i className="fab fa-vimeo-square" />
+          </a>
+        </div>
+        <div className="copyright-wrapper">
+          &copy; 2019 KelseyCreations &#124; all rights reserved
+        </div>
+      </div>
+    );
+  }
+}
