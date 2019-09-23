70a7cb45aae389b9cd369f2c2059f71a235a5353
Aaron Ingersoll <flex.box.hero@gmail.com>
2019-09-23 12:25:48 -0600

    added contact form for mailChimp

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Form, Button } from "react-bootstrap";
+import Mailchimp from "react-mailchimp-form";
 
 export default function() {
   return (
@@ -17,7 +17,23 @@ export default function() {
       </div>
 
       <div className="contact-grid-wrapper">
-        <div className="contact-info-grid-wrapper">
+        <div className="contact-me-grid-wrapper">
+          <div className="company-details-wrapper">
+            <div>
+              <i className="fas fa-map-marker-alt" /> Provo, Utah
+            </div>
+          </div>
+
+          <div className="company-details-wrapper">
+            <div>
+              <i className="fas fa-phone-volume" /> 480.495.5738
+            </div>
+          </div>
+          <div className="company-details-wrapper">
+            <div>
+              <i class="fas fa-paper-plane" /> Contact Me:
+            </div>
+          </div>
           <div className="logo">
             <img
               src="https://kelseycreationsimages.s3-us-west-2.amazonaws.com/good-logo.png"
@@ -25,65 +41,52 @@ export default function() {
             />
           </div>
         </div>
-        <div>
-          <div className="contact-me-grid-wrapper">
-            <div className="company-details-wrapper">
-              <div>
-                <i className="fas fa-map-marker-alt" /> Provo, Utah
-              </div>
-            </div>
 
-            <div className="company-details-wrapper">
-              <div>
-                <i className="fas fa-phone-volume" /> 480.495.5738
-              </div>
-            </div>
-            <div className="inquiries">
-              {" "}
-              <i className="fas fa-envelope" /> Email me here:{" "}
-            </div>
-            <form
-              method="post"
-              action="mailto:aingy205@gmail.com.com?subject=fromKelseyCreations.com"
-            >
-              <input
-                className="submitBTN"
-                type="submit"
-                value="Email &#xf1d8;"
-              />
-            </form>
-          </div>
-          {/* <Form
-  
-          >
-            <div className="two-column">
-              <Form.Group controlId="formBasic">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="First Name" />
-              </Form.Group>
-              <Form.Group controlId="formBasic">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Last Name" />
-              </Form.Group>
-            </div>
-            <div className="two-column">
-              <Form.Group controlId="formBasic">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter Email" />
-              </Form.Group>
-
-              <Form.Group controlId="formBasic">
-                <Form.Label>Phone Number</Form.Label>
-                <Form.Control type="phone" placeholder="Phone Number" />
-              </Form.Group>
-            </div>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Drop Me A Line </Form.Label>
-              <Form.Control as="textarea" rows="3" />
-            </Form.Group>
-            <Button variant="primary" type="submit"  />
-            Email Here
-          </Form> */}
+        <div className="form">
+          <Mailchimp
+            action="https://gmail.us20.list-manage.com/subscribe/post?u=6bdbb9c6b316a597c70a42fa9&amp;id=d6738e58a4"
+            fields={[
+              {
+                name: "FNAME",
+                placeholder: "First Name",
+                type: "text",
+                required: true
+              },
+              {
+                name: "LNAME",
+                placeholder: "Last Name",
+                type: "text",
+                required: true
+              },
+              {
+                name: "EMAIL",
+                placeholder: "Email",
+                type: "email",
+                required: true
+              },
+              {
+                name: "PHONE",
+                placeholder: "Phone #",
+                type: "phone",
+                required: true
+              },
+              {
+                name: "MESSAGE",
+                placeholder: "Ask me about Kelsey Creations!",
+                type: "text",
+                required: true
+              }
+            ]}
+            messages={{
+              sending: "Sending...",
+              success: "Thank you for Contacting me!",
+              error: "An unexpected internal error has occurred.",
+              empty: "You must write an e-mail.",
+              duplicate: "Too many subscribe attempts for this email address",
+              button: "Contact me!"
+            }}
+            className="Form"
+          />
         </div>
       </div>
     </div>
